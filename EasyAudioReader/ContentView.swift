1e058f3eb09cb5bceca8e3c4de9fa932cf10103b
J. MOREAU <leeloomulti@gmail.com>
2020-06-15 21:53:36 +0200

    Initial Commit

---
+++ EasyAudioReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EasyAudioReader
+//
+//  Created by Jordan MOREAU on 15/06/2020.
+//  Copyright © 2020 Jordan MOREAU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
